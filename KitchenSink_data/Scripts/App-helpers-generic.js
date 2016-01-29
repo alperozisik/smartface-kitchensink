3f9219b6607c23d9b3e865daa6e3819065b7161e
alperozisik <alper.ozisik@smartface.io>
2016-01-29 14:59:30 +0200

    Changed to v5

--- KitchenSink_data/Scripts/App-helpers-generic.js
+++
@@ -1,9 +0,0 @@
-App.helpers.generic.generateEventCallbacks = function(eventNames, callback){
-    var events = {};
-    _.each(eventNames, function(eventName) {
-      events[eventName] = function(e){
-          callback(eventName);
-      };
-    });
-    return events;
-};
---
+++ scripts/Page1.Smartface.js
@@ -0,0 +1,32 @@
+Pages.Page1 = new SMF.UI.Page({
+  onShow: Page1_Self_OnKeyPress,
+  onKeyPressed: Page1_Self_OnKeyPress
+});
+/**
+* Creates action(s) that are run when the user press the key of the devices.
+* @param {KeyCodeEventArguments} e Uses to for key code argument. It returns e.keyCode parameter.
+* @this SMF.UI.Page
+*/
+function Page1_Self_OnKeyPress(e) {
+    if (e.keyCode === 4) {
+        Application.exit();
+    }
+}
+/**
+* Creates action(s) that are run when the page is appeared
+* @param {EventArguments} e Returns some attributes about the specified functions
+* @this SMF.UI.Page
+*/
+function Page1_Self_OnShow() {
+    //Comment following block for removing navigationbar/actionbar sample
+    //Copy this code block to every page onShow
+    /*header.init(this);
+    header.setTitle("Page1");
+    header.setRightItem("RItem");
+    header.setLeftItem();*/
+    /**/
+    //Because we are not entering from Page1
+    //Could have changed this from defaults.xml
+    setTimeout(function(){ Pages['UI-Elements.index'].show(); }, 250);
+    setTimeout(function(){ Page1_Self_OnShow = function(){ Application.exit(); } }, 750);
+}
